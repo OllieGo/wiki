ec18b43c551d18ee11758635de423317c5172a7d
OllieGo <olliego@126.com>
2022-02-10 23:21:09 +0800

    增加通用返回类CommonResp

--- src/main/java/com/olliego/wiki/controller/EbookController.java
+++ src/main/java/com/olliego/wiki/controller/EbookController.java
@@ -2,6 +2,7 @@ package com.olliego.wiki.controller;
 
 
 import com.olliego.wiki.model.Ebook;
+import com.olliego.wiki.resp.CommonResp;
 import com.olliego.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setData(list);
+        return resp;
     }
 }
