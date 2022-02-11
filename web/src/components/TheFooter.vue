281271f7c1cd53a2907f3d4277bae6cbdf91adf1
OllieGo <olliego@126.com>
2022-02-11 23:17:34 +0800

    制作TheFooter组件

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki ©2022 Created by OllieGo
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'TheFooter'
+    });
+</script>
