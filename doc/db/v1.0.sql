a4e12608064e5b2994da1a61416e6390e69d913c
OllieGo <olliego@126.com>
2022-02-23 23:48:46 +0800

    电子书管理页面

--- doc/db/v1.0.sql
+++ doc/db/v1.0.sql
@@ -2,25 +2,41 @@
 drop table if exists `ebook`;
 
 CREATE TABLE `ebook` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键Id',
-  `create_people` varchar(45) DEFAULT NULL COMMENT '创建人',
-  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `modify_people` varchar(45) DEFAULT NULL COMMENT '修改人',
-  `modify_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  `deleted` int(1) DEFAULT '0' COMMENT '是否删除 0正常 1删除',
+                         `id`              bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键Id',
+                         `create_people`   varchar(45) DEFAULT NULL COMMENT '创建人',
+                         `create_time`     datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                         `modify_people`   varchar(45) DEFAULT NULL COMMENT '修改人',
+                         `modify_time`     datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+                         `deleted`         int(1) DEFAULT '0' COMMENT '是否删除 0正常 1删除',
 
-  `name` varchar(45) NOT NULL COMMENT '名称',
-  `category_one_id` bigint(20) DEFAULT NULL COMMENT '分类一',
-  `category_two_id` bigint(20) DEFAULT NULL COMMENT '分类二',
-  `description` varchar(200) DEFAULT NULL COMMENT '描述',
-  `cover` varchar(200) DEFAULT NULL COMMENT '封面',
-  `doc_count` int(10) DEFAULT '0' COMMENT '文档数',
-  `view_count` int(10) DEFAULT '0' COMMENT '阅读数',
-  `vote_count` int(10) DEFAULT '0' COMMENT '点赞数',
-  PRIMARY KEY (`id`)
+                         `name`            varchar(45) NOT NULL COMMENT '名称',
+                         `category_one_id` bigint(20) DEFAULT NULL COMMENT '分类一',
+                         `category_two_id` bigint(20) DEFAULT NULL COMMENT '分类二',
+                         `description`     varchar(200) DEFAULT NULL COMMENT '描述',
+                         `cover`           varchar(200) DEFAULT NULL COMMENT '封面',
+                         `doc_count`       int(10) DEFAULT '0' COMMENT '文档数',
+                         `view_count`      int(10) DEFAULT '0' COMMENT '阅读数',
+                         `vote_count`      int(10) DEFAULT '0' COMMENT '点赞数',
+                         PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1253550049971695648 DEFAULT CHARSET=utf8mb4 COMMENT='电子书';
 
-insert into `ebook` (id, name, description) values (1, 'SpringBoot入门教程', '零基础入门Java开发');
-insert into `ebook` (id, name, description) values (2, 'Vue入门教程', '零基础入门Vue开发');
-insert into `ebook` (id, name, description) values (3, 'Mysql入门教程', '零基础入门Mysql开发');
-insert into `ebook` (id, name, description) values (4, 'Python入门教程', '零基础入门Python开发');
+INSERT INTO `ebook`(`id`, `create_people`, `create_time`, `modify_people`, `modify_time`, `deleted`, `name`,
+                    `category_one_id`, `category_two_id`, `description`, `cover`, `doc_count`, `view_count`,
+                    `vote_count`)
+VALUES (1, NULL, '2022-02-10 23:06:52', NULL, '2022-02-23 23:09:13', 0, 'SpringBoot入门教程', NULL, NULL, '零基础入门Java开发',
+        '/image/cover2.png', 0, 0, 0);
+INSERT INTO `ebook`(`id`, `create_people`, `create_time`, `modify_people`, `modify_time`, `deleted`, `name`,
+                    `category_one_id`, `category_two_id`, `description`, `cover`, `doc_count`, `view_count`,
+                    `vote_count`)
+VALUES (2, NULL, '2022-02-10 23:06:52', NULL, '2022-02-23 23:09:18', 0, 'Vue入门教程', NULL, NULL, '零基础入门Vue开发',
+        '/image/cover1.png', 0, 0, 0);
+INSERT INTO `ebook`(`id`, `create_people`, `create_time`, `modify_people`, `modify_time`, `deleted`, `name`,
+                    `category_one_id`, `category_two_id`, `description`, `cover`, `doc_count`, `view_count`,
+                    `vote_count`)
+VALUES (3, NULL, '2022-02-10 23:06:52', NULL, '2022-02-10 23:06:52', 0, 'Mysql入门教程', NULL, NULL, '零基础入门Mysql开发', NULL,
+        0, 0, 0);
+INSERT INTO `ebook`(`id`, `create_people`, `create_time`, `modify_people`, `modify_time`, `deleted`, `name`,
+                    `category_one_id`, `category_two_id`, `description`, `cover`, `doc_count`, `view_count`,
+                    `vote_count`)
+VALUES (4, NULL, '2022-02-10 23:06:52', NULL, '2022-02-10 23:06:52', 0, 'Python入门教程', NULL, NULL, '零基础入门Python开发', NULL,
+        0, 0, 0);
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,9 +3,154 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <a-table
+          :columns="columns"
+          :row-key="record => record.id"
+          :data-source="ebooks"
+          :pagination="pagination"
+          :loading="loading"
+          @change="handleTableChange"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar"/>
+        </template>
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <router-link :to="'/admin/doc?ebookId=' + record.id">
+              <a-button type="primary">
+                文档管理
+              </a-button>
+            </router-link>
+            <a-button type="primary" @click="edit(record)">
+              编辑
+            </a-button>
+            <a-popconfirm
+                title="删除后不可恢复，确认删除?"
+                ok-text="是"
+                cancel-text="否"
+                @confirm="handleDelete(record.id)"
+            >
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-popconfirm>
+          </a-space>
+        </template>
+      </a-table>
       <div class="about">
         <h1>电子书管理</h1>
       </div>
     </a-layout-content>
   </a-layout>
 </template>
+
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue';
+import axios from 'axios';
+import {message} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const param = ref();
+    param.value = {};
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 2,
+      total: 0
+    });
+    const loading = ref(false);
+
+    const columns = [
+      {
+        title: '封面',
+        dataIndex: 'cover',
+        slots: {customRender: 'cover'}
+      },
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '分类',
+        slots: {customRender: 'category'}
+      },
+      {
+        title: '文档数',
+        dataIndex: 'docCount'
+      },
+      {
+        title: '阅读数',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
+      }
+    ];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      ebooks.value = [];
+      axios.post("/ebook/list", {
+        params: {
+          page: params.page,
+          size: params.size,
+          name: param.value.name
+        }
+      }).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          ebooks.value = data.content.list;
+
+          // 重置分页按钮
+          pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    /**
+     * 表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({});
+    });
+
+    return {
+      param,
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange
+    }
+  }
+});
+
+
+</script>
