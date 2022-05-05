bf30c46b95ab120e9108551e7664e740613a7c97
OllieGo <olliego@126.com>
2022-05-05 23:52:50 +0800

    vuex整合sessionStorage，解决刷新数据丢失问题

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,12 +1,22 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
-export default createStore({
+declare let SessionStorage: any;
+const USER = "USER";
+
+const store = createStore({
   state: {
+    user: SessionStorage.get(USER) || {}
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user;
+      SessionStorage.set(USER, user);
+    }
   },
   actions: {
   },
   modules: {
   }
 })
+
+export default store;
